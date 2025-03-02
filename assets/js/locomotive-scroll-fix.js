474c3a6512940d68f14fd585161aef75387358c3
Laura Pais <pais.laura@gmail.com>
2025-03-02 22:27:46 +0000

    ++

---
+++ assets/js/locomotive-scroll-fix.js
@@ -0,0 +1,8 @@
+var scroller = new LocomotiveScroll({
+        el: document.querySelector('[data-scroll-container]'),
+        smooth: true
+    });
+
+new ResizeObserver(() => scroller.update()).observe(document.querySelector("[data-scroll-container]"));
+
+AOS.init();
